39dfb464a40c78da91536dcce0d669cbca522177
ShibilPM <shibilpm3232@gmail.com>
2024-05-23 22:28:02 +0530

    Created board and placed pieces.

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -2,3 +2,21 @@
 Responsible for storing all the information about the current state of the chess game.
 And determining the valid moves at the current state.
 """
+class GameState():
+    def __init__(self):
+        # board is 8x8 2d list, each element of the list has 2 char
+        # first char -> color (b or w)
+        # second char -> the piece
+        # empty space -> "--"
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
